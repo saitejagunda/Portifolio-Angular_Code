bc4213620505d632c5cc0abad28a816cb02ce525
Sai Teja Gunda <saitejagunda@gmail.com>
2020-03-01 17:29:10 +0530

    code

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My-Manager';
+  dev1=['Ansible','Kubernetes','Docker','Git & Gitlab']
+  dev2=['Jenkins','Azure','Hadoop','Python']
+  dev3=['Ubuntu','Maven','SonarQube','Linux']
+}
